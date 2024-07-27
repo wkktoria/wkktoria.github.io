8a1a642963c3f5c318d867dcce3b0b682bfd3b18
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-07-27 18:59:56 +0200

    Add Polish translation

---
+++ scripts/translation.js
@@ -0,0 +1,96 @@
+const aboutLink = document.getElementById("about-link");
+const projectsLink = document.getElementById("projects-link");
+const skillsLink = document.getElementById("skills-link");
+const contactLink = document.getElementById("contact-link");
+const languageSelect = document.getElementById("language-select");
+const englishOption = document.getElementById("english-option");
+const polishOption = document.getElementById("polish-option");
+const greeting = document.getElementById("greeting");
+const linksNote = document.getElementById("links-note");
+const aboutHeading = document.getElementById("about-heading");
+const aboutDescription = document.getElementById("about-description");
+const projectsHeading = document.getElementById("projects-heading");
+const projectsDescription = document.getElementById("projects-description");
+const skillsHeading = document.getElementById("skills-heading");
+const skillsDescription = document.getElementById("skills-description");
+const contactHeading = document.getElementById("contact-heading");
+const contactSendMe = document.getElementById("contact-send-me");
+const contactReachMe = document.getElementById("contact-reach-me");
+
+languageSelect.addEventListener("change", (e) => {
+  localStorage.setItem("lang", e.target.value);
+  loadText();
+  console.log(localStorage.getItem("lang"));
+});
+
+const translations = {
+  en: {
+    about: "about",
+    aboutDescription:
+      "i'm dedicated and passionate computer science student, seeking to gain more programming experience and face new challenges",
+    projects: "projects",
+    projectsDescription:
+      "i'm into open-source, so all of my projects are available on",
+    skills: "skills",
+    skillsDescription: "there are languages and tools that I use",
+    contact: "contact",
+    contactEmail: "feel free to send me",
+    contactSocials: "or reach me on my socials",
+    english: "english",
+    polish: "polish",
+    greeting: "hello, i'm wkktoria! 👋",
+    linksNote: "note: all the links below open in new tabs",
+  },
+  pl: {
+    about: "o mnie",
+    aboutDescription:
+      "jestem zaangażowaną i pełną pasji studentką informatyki, która chce zdobyć więcej doświadczenia w programowaniu i stawić czoła nowym wyzwaniom",
+    projects: "projekty",
+    projectsDescription:
+      "jestem zwolenniczką open-source, dlatego wszystkie moje projekty są dostępne na",
+    skills: "umiejętności",
+    skillsDescription: "języki programowania i narzędzia, których używam",
+    contact: "kontakt",
+    contactEmail: "możesz wysłać mi",
+    contactSocials:
+      "lub skontaktować się ze mną na moich portalach społecznościowych",
+    english: "angielski",
+    polish: "polski",
+    greeting: "cześć, jestem wkktoria! 👋",
+    linksNote: "uwaga: wszystkie linki poniżej otwierają się w nowych kartach",
+  },
+};
+
+const i18n = (key) => {
+  const language = localStorage.getItem("lang") ?? "en";
+  return translations[language][key];
+};
+
+const loadText = () => {
+  aboutLink.innerText = i18n("about");
+  projectsLink.innerText = i18n("projects");
+  skillsLink.innerText = i18n("skills");
+  contactLink.innerText = i18n("contact");
+
+  englishOption.innerText = i18n("english");
+  polishOption.innerText = i18n("polish");
+
+  greeting.innerText = i18n("greeting");
+
+  linksNote.innerText = i18n("linksNote");
+
+  aboutHeading.innerText = i18n("about");
+  aboutDescription.innerText = i18n("aboutDescription");
+
+  projectsHeading.innerText = i18n("projects");
+  projectsDescription.innerText = i18n("projectsDescription");
+
+  skillsHeading.innerText = i18n("skills");
+  skillsDescription.innerText = i18n("skillsDescription");
+
+  contactHeading.innerText = i18n("contact");
+  contactSendMe.innerText = i18n("contactEmail");
+  contactReachMe.innerText = i18n("contactSocials");
+};
+
+loadText();
