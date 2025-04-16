b6a618234eca5825f812f7238d4917613fb55780
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-04-16 23:21:15 +0200

    Refactor

--- src/sections/experience/AllExperience.jsx
+++ src/sections/experience/AllExperience.jsx
@@ -1,31 +1,7 @@
 import React from "react";
 import SingleExperience from "./SingleExperience";
 import { FaArrowRight } from "react-icons/fa";
-
-const experiences = [
-  {
-    job: "Student",
-    company: "Wyższa Szkoła Biznesu (WSB-NLU) in Nowy Sącz",
-    date: "2022 - 2026",
-    responsibilities: [
-      "Mastering core concepts.",
-      "Building practical skills.",
-      "Staying current with technology.",
-      "Collaborating and networking.",
-    ],
-  },
-  {
-    job: "Fullstack Developer Internship",
-    company: 'Fundacja na Rzecz Promocji i Rozwoju "Głos Młodych"',
-    date: "April 2025 - January 2026",
-    responsibilities: [
-      "Creation and administration of websites.",
-      "Creation, administration of relational databases.",
-      "Web application development.",
-      "Testing and ensuring the quality of developed software.",
-    ],
-  },
-];
+import { experiences } from "../../constants/experiences";
 
 const AllExperience = () => {
   return (
